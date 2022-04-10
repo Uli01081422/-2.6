208f2cdacd74ab9db799d61027b4efaf48f5cb77
Uli01081422 <100164469+Uli01081422@users.noreply.github.com>
2022-04-10 12:39:54 +0300

    Add files via upload

---
+++ ООП 2.6/ООП 2.6/ООП 2.6/Complex.cpp
@@ -0,0 +1,105 @@
+#include "Complex.h"
+using namespace std;
+
+void Complex::Init(Pair pair) { SetPair(pair); }
+
+void Complex::Display(Pair& k1, Pair& k2)
+{
+    Complex R;
+    cout << " (a,b) * (c,d) = (a c * b d , a d + b c)" << "= ";
+    cout << "( " << (R.Multiply(k1, k2)) << " , " << (R.Multiply(k1, k2)) << " )" << endl;
+
+    cout << " (a,b) - (c,d) = (a - c, b - d)" << "=";
+    cout << "( " << R.Minus(k1, k2) << " , " << R.Minus(k1, k2) << " )" << endl;
+}
+
+int Complex::Minus (Pair& k1, Pair& k2)
+{
+    return (k1.GetA() - k2.GetB(), k1.GetA() - k2.GetB());
+}
+
+
+int Complex::Multiply(Pair& k1, Pair& k2)
+{
+    return (k1.GetA() * k2.GetB() - k2.GetA()) * (k1.GetB(), k2.GetB(), k1.GetA() * k2.GetB() - k2.GetA()) * (k1.GetB(), k2.GetB());
+}
+
+
+Complex::Pair::Pair()
+{
+    first = 0;
+    second = 0;
+}
+Complex::Pair::Pair(int d = 0, int v = 0)
+{
+    first = d;
+    second = v;
+}
+Complex::Pair::Pair(const Pair& r)
+{
+    first = r.first;
+    second = r.second;
+}
+Complex::Pair::~Pair() { }
+
+void Complex::Pair::Init(int D, int V)
+{
+    SetFirst(D);
+    SetSecond(V);
+}
+
+Complex::Pair operator + (const Complex::Pair& a, const Complex::Pair& b)
+{
+    return Complex::Pair(a.first + b.first, a.second + b.second);
+}
+
+Complex::Pair& Complex::Pair::operator = (const Complex::Pair& r)
+{
+    first = r.first;
+    second = r.second;
+    return *this;
+}
+
+Complex::Pair::operator string () const
+{
+    stringstream sstr;
+    sstr << endl << " ( " << first << " , " << second << " )" << endl << endl;
+    return sstr.str();
+}
+
+ostream& operator << (ostream& out, const Complex::Pair& r)
+{
+    out << string(r);
+    return out;
+}
+
+istream& operator >> (istream& in, Complex::Pair& r)
+{
+    cout << " "; in >> r.first;
+    cout << " "; in >> r.second;
+    r.Init(r.first, r.second);
+    return in;
+}
+
+Complex::Pair& Complex::Pair::operator ++()
+{
+    first++;
+    return *this;
+}
+Complex::Pair& Complex::Pair::operator --()
+{
+    first--;
+    return *this;
+}
+Complex::Pair Complex::Pair::operator ++(int)
+{
+    Pair t(*this);
+    second++;
+    return t;
+}
+Complex::Pair Complex::Pair::operator --(int)
+{
+    Pair t(*this);
+    second--;
+    return t;
+}
